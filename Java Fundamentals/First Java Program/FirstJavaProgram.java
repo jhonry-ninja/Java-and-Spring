99606b94fe927c8edafe08a77c7d40a358f8b773
jhonry-ninja <jcopinaldo@gmail.com>
2020-03-24 13:58:04 -0700

    Add files via upload

---
+++ Java Fundamentals/First Java Program/FirstJavaProgram.java
@@ -0,0 +1,27 @@
+// First Java Program
+
+/* In this assignment, you will create your own Java Program 
+that will print your name, age, and hometown. Once you run your 
+bytecode, you should see the following output: */
+My name is Jhonry Opinaldo.
+I am 100 years old.
+My hometown is Milpitas, CA.
+
+/* Objectives:
+● Learn about the java program life cycle.
+● Start coding java right away.
+
+Tasks:
+● Create a new source file inside of your javaFun directory.
+● In the main method, implement the code to print your name, 
+  age, and hometown in different lines.
+● Compile your code without errors.
+● Run your .class file. */
+
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Jhonry Opinaldo.");
+        System.out.println("I am 100 years old.");
+        System.out.println("My hometown is Milpitas, CA.");
+    }
+}
