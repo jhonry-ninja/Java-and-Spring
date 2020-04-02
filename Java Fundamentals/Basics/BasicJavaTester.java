e856d3f13a14d0a196e1c0f23ec9a3e4f1275aa2
jhonry-ninja <jcopinaldo@gmail.com>
2020-04-02 09:45:01 -0700

    Add files via upload

---
+++ Java Fundamentals/Basics/BasicJavaTester.java
@@ -0,0 +1,9 @@
+public class BasicJavaTester{
+    public static void main(String[] args) {
+        BasicJava b = new BasicJava();
+        // String s = "";
+        // int i = 0;
+        b.print1To255();
+        b.printSum();
+    }
+}
